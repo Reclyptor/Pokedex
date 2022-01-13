b73303d35de535361283fdcafb44a58a3a36d5e8
Emilio Torres Jr <emiliotorres3193@gmail.com>
2022-01-13 01:00:08 -0600

    Added Pokedex Caching

--- repository/pokedex.go
+++ repository/pokedex.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/patrickmn/go-cache"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -28,7 +29,12 @@ func (pokedexRepository PokedexRepository) FindPokedexes () []model.Pokedex {
 }
 
 func (pokedexRepository PokedexRepository) FindPokedexByID(id int) model.Pokedex {
-	var pokedex model.Pokedex
-	_ = pokedexRepository.db.Where(model.Pokedex{PokedexID: id}).Preload(clause.Associations).Preload("Region.Subregions").Preload("Entries.Pokemon").Preload("Entries.Pokemon.Types").Preload("Entries.Pokemon.Physique").Preload("Entries.Pokemon.Abilities").Preload("Entries.Pokemon.Statistics").Preload("Entries.Pokemon.Training").Preload("Entries.Pokemon.Images").Take(&pokedex)
-	return pokedex
+	if pokedex, found := pokedexRepository.cache.Get(fmt.Sprintf("pokedex|%d", id)); found {
+		return pokedex.(model.Pokedex)
+	} else {
+		var pokedex model.Pokedex
+		_ = pokedexRepository.db.Where(model.Pokedex{PokedexID: id}).Preload(clause.Associations).Preload("Region.Subregions").Preload("Entries.Pokemon").Preload("Entries.Pokemon.Types").Preload("Entries.Pokemon.Physique").Preload("Entries.Pokemon.Abilities").Preload("Entries.Pokemon.Statistics").Preload("Entries.Pokemon.Training").Preload("Entries.Pokemon.Images").Take(&pokedex)
+		pokedexRepository.cache.Set(fmt.Sprintf("pokedex|%d", id), pokedex, cache.DefaultExpiration)
+		return pokedex
+	}
 }
