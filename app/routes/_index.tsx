56217abfbd615d0079989fa02d0d37d26eb30c90
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-07-31 03:46:11 -0500

    Updated Routes + Added Layout Component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,31 @@
-import { Outlet } from "react-router";
+import {Outlet, useNavigate} from "react-router";
+import {useEffect, useMemo} from "react";
+import Main from "~/layout/Main";
 
 const Index = () => {
-  return <Outlet />;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/pokedex");
+  }, []);
+
+  const header = useMemo(() => {
+    return (
+      <span className="text-center text-[10px] py-1 w-fit leading-[10px]">PokemonChamber</span>
+    );
+  }, []);
+
+  const footer = useMemo(() => {
+    return (
+      <span className="text-center text-[10px] py-1 w-fit leading-[10px]">PokemonChamber is a fan site unaffiliated with The Pokémon Company. All Pokémon content and materials are trademarks and copyrights of The Pokémon Company International, Inc.</span>
+    );
+  }, []);
+
+  return (
+    <Main header={ header } footer={ footer } className="w-screen h-screen [&>header]:border-b [&>header]:border-b-border [&>footer]:border-t [&>footer]:border-t-border">
+      <Outlet />
+    </Main>
+  );
 };
 
 export default Index;
---
+++ app/routes/pokedex.tsx
@@ -0,0 +1,7 @@
+const Pokedex = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Pokedex;
