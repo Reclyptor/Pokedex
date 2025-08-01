af320a173e928938964074889dcc633b343648e9
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-07-31 22:23:56 -0500

    Added Sidebar Line Design

--- app/component/Sidebar.tsx
+++ app/component/Sidebar.tsx
@@ -10,7 +10,7 @@ export type SidebarProps = {
 const Sidebar = (props: SidebarProps) => {
   return !props.open ? null : (
     <div className={ twMerge(clsx("flex flex-col w-[256px] h-full bg-red", props.className)) }>
-      <div className="flex items-center w-full h-[56px] px-6 gap-x-4">
+      <div className="flex items-center w-full h-[54px] px-6 gap-x-4">
         <div className="w-fit h-fit rounded-full border-3 border-black bg-black/80">
           <div className="w-fit h-fit rounded-full border-2 border-primary bg-black/80">
             <div className="relative w-6 h-6 rounded-[inherit] bg-blue animate-[blink_2s_ease_infinite_0s]">
@@ -36,7 +36,10 @@ const Sidebar = (props: SidebarProps) => {
           </div>
         </div>
       </div>
-      <div className="relative w-full flex-grow border-r-2 border-r-black box-content">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -55 256 56" className="border-r-2 border-r-black">
+        <path d="m 0 0 L 100 0 C 108 0 148 -54 156 -54 L 256 -54" stroke="#000000" stroke-width="2" fill="none"/>
+      </svg>
+      <div className="relative w-full flex-grow border-r-2 border-r-black">
         <div className="absolute top-1/2 right-0 bottom-0 left-0 bg-[linear-gradient(to_bottom,rgba(0,0,0,0),rgba(0,0,0,.6))] pointer-events-none z-[1]" />
       </div>
     </div>
