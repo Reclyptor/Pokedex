de16c7252ee0a1163e26d8f266b5fb0a41e52ba2
Reclyptor <5952751+Reclyptor@users.noreply.github.com>
2025-07-31 23:19:29 -0500

    Added Content Cover

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,7 @@ import { Outlet, useNavigate } from "react-router";
 import { useEffect, useMemo } from "react";
 import Sidebar from "~/component/Sidebar";
 import Main from "~/layout/Main";
+import Cover from "~/component/Cover";
 
 const Index = () => {
   const navigate = useNavigate();
@@ -24,13 +25,15 @@ const Index = () => {
 
   const footer = useMemo(() => {
     return (
-      <span className="flex items-center justify-center w-full text-neutral text-center text-[10px] py-1 leading-[10px] bg-background-dark">This fan site is not affiliated with The Pokémon Company. All Pokémon content and materials are trademarks and copyrights of The Pokémon Company International, Inc.</span>
+      <span className="flex items-center justify-center w-full text-black text-center text-[10px] py-1 leading-[10px] bg-red border-t-2 border-t-black">This fan site is not affiliated with The Pokémon Company. All Pokémon content and materials are trademarks and copyrights of The Pokémon Company International, Inc.</span>
     );
   }, []);
 
   return (
     <Main sidebar={ sidebar } header={ header } footer={ footer } className="w-screen h-screen bg-lcd-dark/75">
-      <Outlet />
+      <Cover open className="w-full h-full">
+        <Outlet />
+      </Cover>
     </Main>
   );
 };
